542ecef80d92159bffc9cf53a02d79e0e8567dce
GOBLET Romain <romain.goblet55@gmail.com>
2018-04-20 18:26:43 +0200

    Script déplacement

---
+++ Prototype/Assets/mouvement_balle.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mouvement_balle : MonoBehaviour
+{
+	void Update()
+	{
+		var x = Input.GetAxis("Horizontal") * Time.deltaTime * 250.0f;
+		var z = Input.GetAxis("Vertical") * Time.deltaTime * 5.0f;
+
+		transform.Rotate(0, x, 0);
+		transform.Translate(0, 0, z);
+	}
+}
