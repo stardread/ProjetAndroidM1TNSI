d2f09182ec326959df0a025f3640e0c7b70c25e6
Kevin B <bontemps.kevin1@gmail.com>
2018-04-20 19:07:20 +0200

    Ajout controller event pour bouton

---
+++ Prototype/Assets/controllerMouv.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class controllerMouv : MonoBehaviour {
+
+	public Transform target;
+	public float speed = 5;
+
+	bool _rightButtonDown;
+	bool _leftButtonDown;
+
+	// Update is called once per frame
+	void Update ()
+	{
+		if (_rightButtonDown) {
+			float moveRight = (Time.deltaTime * speed);
+			target.Translate (moveRight, 0, 0);
+		}
+		if (_leftButtonDown) {
+			float moveLeft = (Time.deltaTime * speed);
+			target.Translate (-moveLeft, 0, 0);
+		}
+	}
+
+	public void OnRightButtonDown (bool down)
+	{
+		_rightButtonDown = down;
+	}
+
+	public void OnLeftButtonDown (bool down)
+	{
+		_leftButtonDown = down;
+	}
+}
