2c7db6784e0ddeee3f13d3bc7c86b6a42a5ac8a2
GOBLET Romain <romain.goblet55@gmail.com>
2018-05-10 20:00:54 +0200

    Ajout plateforme victoire

--- Prototype/Assets/collision_win.cs
+++ Prototype/Assets/collision_win.cs
@@ -2,9 +2,15 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class mouvement_camera : MonoBehaviour {
+public class collision_win : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
 	// Update is called once per frame
 	void Update () {
-		transform.Translate (Vector3.forward * Time.deltaTime * 3.5f);
+		
 	}
 }
---
+++ Prototype/Assets/collision_win2.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class collision_win2 : MonoBehaviour {
+
+
+
+	public GUISkin mySkin;
+	public Texture2D image_attention;
+	private bool flag = false;
+	private bool isTrigger = false;
+	public Texture2D fond;	
+	private Rigidbody rb;
+
+
+	// Use this for initialization
+	void Start () 
+	{
+		flag = false;
+	}
+
+	void OnCollisionEnter(Collision collision)  
+	{
+		if (collision.gameObject.name == "Arrivée") {
+			flag = true;
+			Debug.Log ("Message collision visible");
+		}
+	}
+
+	/*void OnCollisionExit(Collision collision)
+	{
+		if (collision.gameObject.name == "Terrain") {
+			flag = false;
+			Debug.Log ("Message collision caché");
+		}
+	}*/
+
+	void OnGUI()
+	{
+
+		GUI.skin = mySkin;
+
+		//fond = (Texture2D)UnityEditor.AssetDatabase.LoadAssetAtPath("Assets/Textures/wasted.png", typeof(Texture2D));
+		int l = 750; //largeur panneau
+		int h = 750; //hauteur panneau
+		int ox = Screen.width/2-l/2; //position panneau pour le centrer en X
+		int oy = Screen.height/2-h/2; //position panneau pour le centrer en Y
+
+		if(flag == true)
+		{  	
+			//Affichage fond noir
+			GUI.color = new Color( 1,1,1, 0.7f); //Permet d'afficher la DrawTexture avec de la transparence
+			//GUI.DrawTexture(new Rect(0,0 ,Screen.width,Screen.height), fond);
+			GUI.color = new Color( 1,1,1,1); //Permet de remettre la transparence à 1 pour les GUI suivants
+
+			GUI.TextField(new Rect(ox,oy,l,h),"VICOIRE !");
+		}
+	}
+}
