af4e82ea162b2f6f7c869e7d24b39a708bdaf7c2
GOBLET Romain <romain.goblet55@gmail.com>
2018-05-10 16:10:26 +0200

    Parcours modifié

--- Prototype/Assets/mouvement_plateau.cs
+++ Prototype/Assets/mouvement_plateau.cs
@@ -6,6 +6,6 @@ public class mouvement_plateau : MonoBehaviour {
 	public float speed;
 	// Update is called once per frame
 	void Update () {
-		transform.Translate (Vector3.forward * Time.deltaTime * 2);
+		transform.Translate (Vector3.down * Time.deltaTime * 2);
 	}
 }
