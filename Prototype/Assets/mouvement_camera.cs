74891f1612c36e7e0e94bb4d74973fe8bc9f9de2
GOBLET Romain <romain.goblet55@gmail.com>
2018-05-10 16:31:36 +0200

    Mouvement camera

---
+++ Prototype/Assets/mouvement_camera.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mouvement_camera : MonoBehaviour {
+	// Update is called once per frame
+	void Update () {
+		//transform.Translate (Vector3.forward * Time.deltaTime * 2);
+	}
+}
