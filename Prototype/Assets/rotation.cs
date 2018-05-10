6d213d95745b7a8e163ab34c18fe9e3e976f31f2
Kevin B <bontemps.kevin1@gmail.com>
2018-05-10 18:13:50 +0200

    rotation plus mouv ball

---
+++ Prototype/Assets/rotation.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rotation : MonoBehaviour {
+
+	/*public float speed ;
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate(Vector3.left, speed , Space.Self);
+		transform.Translate (Vector3.back * Time.deltaTime * speed);
+
+
+		//transform.Rotate((Vector3.forward* Time.deltaTime), Space.Self);
+	}*/
+	public float speed;
+
+	private Rigidbody rb;
+
+	void Start ()
+	{
+		rb = GetComponent<Rigidbody>();
+	}
+
+	void FixedUpdate ()
+	{
+		
+
+		Vector3 movement = new Vector3 (0.0f,0.0f,-1.0f);
+
+		rb.AddForce (movement * speed);
+	}
+}
