aa93356fd887294d5e8facb7133a9fc418ba7bf2
Kevin B <bontemps.kevin1@gmail.com>
2018-05-16 22:46:41 +0200

    menu demarrer + nouveau bouton restart

--- Prototype/Assets/restart_game.cs
+++ Prototype/Assets/restart_game.cs
@@ -4,8 +4,11 @@ using System.Collections;
 
 public class restart_game : MonoBehaviour {
 
+    public string sceneToLoad;
 	public void RestartGame() {
-		SceneManager.LoadScene(SceneManager.GetActiveScene().name); // loads current scene
-	}
+		//SceneManager.LoadScene(SceneManager.GetActiveScene().name); // loads current scene
+        SceneManager.LoadScene(sceneToLoad); // loads current scene
+
+    }
 
 }
