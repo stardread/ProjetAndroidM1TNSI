fdfdf8d0c2443556071080ca7f37d5f2bec628cb
Kevin B <bontemps.kevin1@gmail.com>
2019-01-05 17:54:07 +0100

    ajout pieces et musique

--- Prototype/Assets/mouvement_camera.cs
+++ Prototype/Assets/mouvement_camera.cs
@@ -3,18 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class mouvement_camera : MonoBehaviour {
-    public GameObject player;
 
-    private Vector3 offset;
-
-    void Start()
-    {
-        //offset = transform.position = player.transform.position;
-        offset = new Vector3(0.0f, 5.0f, 15.0f);
-    }
 	// Update is called once per frame
 	void Update () {
-       transform.position = player.transform.position + offset;
-
-    }
+		GameObject gameControllerObject = GameObject.Find("Ball");
+		collisionFail tmp = gameControllerObject.GetComponent<collisionFail> ();
+		if (tmp.flag_tmp == 0) {
+			transform.Translate (Vector3.forward * Time.deltaTime * 3.5f);
+		}
+	}
 }
