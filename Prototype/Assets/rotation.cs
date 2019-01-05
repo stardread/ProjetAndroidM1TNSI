680f23b4c3adb1261a0ad1daddc41f420641e016
Kevin B <bontemps.kevin1@gmail.com>
2019-01-05 18:19:15 +0100

    mise en commun sons

--- Prototype/Assets/rotation.cs
+++ Prototype/Assets/rotation.cs
@@ -1,10 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+
+using UnityEngine.UI;
 using UnityEngine;
 
 public class rotation : MonoBehaviour {
 
-	/*public float speed ;
+    /*public float speed ;
 	// Update is called once per frame
 	void Update () {
 		transform.Rotate(Vector3.left, speed , Space.Self);
@@ -13,14 +15,22 @@ public class rotation : MonoBehaviour {
 
 		//transform.Rotate((Vector3.forward* Time.deltaTime), Space.Self);
 	}*/
+    public AudioClip GoldSound;
 	public float speed;
-
+    public Text countText;
 	private Rigidbody rb;
+    public Text time;
+    private int count;
+    private float temps;
+    private int timeInt;
 
 	void Start ()
 	{
 		rb = GetComponent<Rigidbody>();
-	}
+        count = 0;
+        temps = 0;
+        SetScore();
+    }
 
 	void FixedUpdate()
 	{
@@ -31,13 +41,38 @@ public class rotation : MonoBehaviour {
 			Vector3 movement = new Vector3 (0.0f, 0.0f, -1.0f);
 			rb.AddForce (movement * speed);
 		}
-	}
+
+        SetTime();
+
+    }
 
     void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("Cube"))
         {
             other.gameObject.SetActive(false);
+            count = count + 1;
+            MakeGoldSound();
+            SetScore();
         }
     }
+
+    void SetScore()
+    {
+        countText.text = "Count: " + count.ToString();
+
+    }
+
+    void SetTime()
+    {
+        timeInt = Mathf.RoundToInt(temps);
+        time.text = ("Temps : " + timeInt + " seconde(s)");
+        temps += Time.deltaTime;
+    }
+
+    public void MakeGoldSound()
+    {
+        AudioSource.PlayClipAtPoint(GoldSound, transform.position);
+    }
+
 }
